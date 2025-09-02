f40b8f61d42c775c5fc4b98f6879b38313428b25
akashgaikwad28 <akash.gaikwad@gmail.com>
2025-09-02 22:06:33 +0530

    unit test created

---
+++ test/test_oparatioin.py
@@ -0,0 +1,16 @@
+from src.math_oparation import add , sub
+
+
+def test_add():
+    
+    assert add(2,3) == 5
+    assert add(-1,1) == 0
+    assert add(5,3) == 8
+    
+    
+def test_sub():
+    assert sub(5,3) == 2
+    assert sub(8,-10) == -2
+    assert sub(5,-5) == 10
+    
+    
